bc4950be5eeeec263fd43d62d1542646c1648b05
Serena <sere.michetti@icloud.com>
2025-11-30 21:41:14 +0100

    completato primo snack

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
-//Crea un array composto da 10 automobili.
-//Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano)//
-//Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.//
-//Infine stampa separatamente i 3 array.
+//- Crea un array composto da 10 automobili.
+//- Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+//- Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+//- Infine stampa separatamente i 3 array.
 
-const automobili = [
-    {marca:'Fiat', modello: 'Panda', alimentazione: 'Benzina'},
-    {marca:'Alfa Romeo', modello: 'Giulia', alimentazione: 'Diesel'},
-    {marca:'Tesla', modello: 'Model 3', alimentazione: 'Elettrica'},
-    {marca:'Toyota', modello: 'Yaris', alimentazione: 'Ibrida'},
-    {marca:'BMW', modello: 'Serie 3', alimentazione: 'Benzina'},
-    {marca:'Renault', modello: 'Clio', alimentazione: 'Metano'},
-    {marca:'Ford', modello: 'Fiesta', alimentazione: 'GPL'},
-    {marca:'Porsche', modello: 911, alimentazione: 'Elettrica'},
-    {marca:'Audi', modello: 'A4', alimentazione: 'Ibrida'},
-    {marca:'Chevrolet', modello: 'Cruze', alimentazione: 'Diesel'}
+const automobile = [
+    {marca: 'Fiat', modello: 'Panda', alimentazione: 'Benzina'},
+    {marca: 'Audi', modello: 'Q5', alimentazione: 'Diesel'},
+    {marca: 'Toyota', modello: 'Prius', alimentazione: 'Ibrida'},
+    {marca: 'Tesla', modello: 'Model Y', alimentazione: 'Elettrica'},
+    {marca: 'Volkswagen', modello: 'Golf', alimentazione: 'Metano'},
+    {marca: 'Alfa Romeo', modello: 'Tonale', alimentazione: 'Benzina'},
+    {marca: 'Hyundai', modello: 'Kona', alimentazione: 'Elettrica'},
+    {marca: 'Ford', modello: 'Puma', alimentazione: 'GPL'},
+    {marca: 'Mercedes-Benz', modello: 'Classe C', alimentazione: 'Diesel'},
+    {marca: 'Porsche', modello: 'Taycan', alimentazione: 'Elettrica'}
 ]
-//Con il metodo filter filtro il tipo di alimentazione dell'auto che mi occorre
-const autoDiesel = automobili.filter(auto => auto.alimentazione === 'Diesel');
-const autoBenzina = automobili.filter(auto => auto.alimentazione === 'Benzina');
-const altraAlimentazione = automobili.filter(auto => auto.alimentazione != 'Benzina' && auto.alimentazione != 'Diesel');
 
+const autoBenzina = automobile.filter(auto => {
+    if(auto.alimentazione === 'Benzina'){
+         return true
+    }return false   
+})
+console.log(autoBenzina)
+
+const autoDiesel = automobile.filter(auto => {
+    if(auto.alimentazione === 'Diesel'){
+        return true
+    } return false
+}
+)
+console.log(autoDiesel)
+
+const altreAlimentazioni = automobile.filter(auto =>{
+    if(auto.alimentazione != 'Benzina' && auto.alimentazione != 'Diesel'){
+        return true
+    }return false
+}
+)
+console.log(altreAlimentazioni)
